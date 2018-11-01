529b46c95cc4435a08c165dcb52f7db4c3de2ef6
mcsmith01 <mcsmith01@alumni.amherst.edu>
2018-11-01 12:13:56 -0400

    Graded 01-Nov-2018 12:13:56 PM

--- Homework6.java
+++ Homework6.java
@@ -20,6 +20,10 @@ public class Homework6 {
 	 * String lexicographically, return false if it does not
 	 */
 	public boolean isBefore(String other) {
+		/* compareTo will return a negative
+		 * number if the first string comes
+		 * before
+		 */
 		if (s.compareTo(other) > 0) {
 			return true;
 		} else {
